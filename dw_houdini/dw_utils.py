4dc77f508f43bf802509e7ce083d5b87dac96f83
Alexis Baudoin <c-alexis_baudoin@nintendo-pictures.co.jp>
2025-08-08 13:33:37 +0900

    houdini utils for hda management

--- dw_houdini/dw_utils.py
+++ dw_houdini/dw_utils.py
@@ -5,10 +5,25 @@ Sources :
 
 import hou
 from typing import Optional, Union, Tuple
+from dw_maya.dw_decorators import acceptString
 
 AUTOUPDATE = hou.updateMode.AutoUpdate
 MANUAL = hou.updateMode.Manual
 ONMOUSEUP = hou.updateMode.OnMouseUp
+
+def get_node_instances(category, node_name):
+    if isinstance(category, basestring):
+        category = str_to_category(category)
+
+    if not isinstance(category, hou.NodeTypeCategory):
+        raise TypeError("Not a valid NodeTypeCategory")
+
+    result = []
+    for type_name, node_type in category.nodeTypes().iteritems():
+        name_part = hou.hda.componentsFromFullNodeTypeName(type_name)[2]
+        if node_name == name_part:
+            result.extend(node_type.instances())
+    return result
 
 def str_to_category(category:str)->hou.NodeTypeCategory:
     """
@@ -27,7 +42,43 @@ def get_node_type_categories()->list:
 def get_sop_node_by_type(node_type:str)->list:
     return hou.sopNodeTypeCategory().nodeType(node_type).instances()
 
-def get_hda_versions(node:hou.Node)->list:
+def has_parm(node: hou.Node, parm_name: str) -> bool:
+    return node.parm(parm_name) is not None
+
+@acceptString("node")
+def preserve_node_parms(node_list:hou.Node, parm_list:list=None)->dict:
+    """
+    function used so we can update HDAs to the latest version and preserve some of the parms
+    """
+    result = {}
+
+    for node in node_list:
+
+        if parm_list:
+            node_path = node.path()
+            result[node_path] = {}
+
+            for parm in parm_list:
+                if has_parm(node, parm):
+                    result[node_path][parm] = node.parm(parm).eval()
+
+    return result
+
+def restore_node_parms(preserve_dict:dict=None):
+    if preserve_dict:
+        for node_path in preserve_dict:
+            node = hou.node(node_path)
+            for parm_name in preserve_dict[node_path]:
+                if has_parm(node, parm_name):
+                    node.parm(parm_name).set(preserve_dict[node_path][parm_name])
+
+def get_current_hda_version(node:hou.Node)->hou.HDADefinition:
+    if isinstance(node, str):
+        node = hou.node(node)
+    node_defintion = node.type().definition()
+    return node_defintion
+
+def get_hda_versions(node:hou.Node)->List[hou.HDADefinition]:
     """
     list all the history of a hda SOP::nodename::1.0, SOP::nodename::1.2
     """
@@ -45,18 +96,21 @@ def get_hda_components(node:hou.Node)->Tuple[str,str,str,str]:
     """
     return hou.hda.componentsFromFullNodeTypeName(node.type().name())
 
-def set_hda_version_to_latest(node:hou.Node) -> None:
+def set_hda_version_to_latest(node: hou.Node) -> str:
     """
     set the node to the latest version
     """
     version_list = get_hda_versions(node)
-    lastest = version_list[-1]
-    latest_name = lastest.nodeType().name()
-    node.changeNodeType(latest_name,
-                        keep_name=False,
-                        keep_parms=False,
-                        keep_network_contents=False,
-                        force_change_on_node_type_match=False)
+    latest = version_list[-1]
+    latest_name = latest.nodeType().name()
+
+    if get_current_hda_version(node) != latest:
+        node.changeNodeType(latest_name,
+                            keep_name=True,
+                            keep_parms=False,
+                            keep_network_contents=False,
+                            force_change_on_node_type_match=False)
+    return latest_name
 
 def get_sop_nodes_with_type(_type:str=None)->list:
     """
